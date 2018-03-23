9957080fef0f2f4fa22247b9346db80ceeafc277
davematias <davematiasei@gmail.com>
2018-03-23 17:16:13 +0000

    more webpack training

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './scss/main.scss';
+import { hello, wazzup } from './js/module';
+
+hello();
+wazzup();
---
+++ src/js/module.js
@@ -0,0 +1,9 @@
+function hello(){
+  console.log('Hello from module');
+}
+
+function wazzup(){
+  console.log('Sup');
+}
+
+export { hello, wazzup };
